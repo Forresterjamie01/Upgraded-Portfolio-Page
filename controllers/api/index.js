b6438fb973c41f54e05716b520bb52e4c4918ae7
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-13 18:09:23 -0400

    updating portfolio page with new index, js and css files.

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const TriplogRoutes = require('./Triplog');
-
-router.use('/users', userRoutes);
-router.use('/triplog', TriplogRoutes);
-
-module.exports = router;
-
--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS portfoliopage_db;
-
-CREATE DATABASE portfoliopage_db;
